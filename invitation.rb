1ecddb42570273b5d5d3a7cc2c0660dce2be6829
rosewhitt <rosewhitt@icloud.com>
2018-06-04 21:10:46 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,31 @@
-# Code your prompts here!
+puts "What is your name?"# Code your prompts here!
 
-# Try starting out with puts'ing a string.
+guest_name = gets.strip
+
+puts "What party are you going to?"# Try starting out with puts'ing a string.
+
+party_name = gets.strip
+
+puts "What is the date of the party?"
+
+date = gets.strip
+
+puts "What time is the party?"
+
+time = gets.strip
+
+puts "Who is hosting the party?"
+
+host_name = gets.strip
+
+puts "When is the RSVP date?"
+
+rsvp_date = gets.strip
+
+puts "Dear #{guest_name},"
+
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than #{rsvp_date}."
+
+puts "Sincerely,"
+
+puts "host_name"
